20c6addf3697609834c338e2258e080e544d8112
sun2525 <tooooku88@gmail.com>
2025-05-25 22:40:27 +0900

    firstcommit

---
+++ src/main/java/in/techcamp/issueapp2/Issueapp2Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issueapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issueapp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp2/Issueapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issueapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
